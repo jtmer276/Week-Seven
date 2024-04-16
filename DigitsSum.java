0373610c6c0467a9e2691578f5d36f56073433a7
jtmer276 <159070824+jtmer276@users.noreply.github.com>
2024-04-15 23:58:49 -0400

    vf

--- DigitsSum.java
+++ DigitsSum.java
@@ -8,23 +8,32 @@ import java.util.*;
 public class DigitsSum{
 
   public static void main(String[] args){
-    int num = 0;
+  int num = 0;
+  int num_length = 0;
+  int num_final = 0;
+  String i = "";
 
-    Scanner input = new Scanner(System.in);
+  Scanner input = new Scanner(System.in);
 
-    System.out.print("Enter a positive integer: ");
-    num = input.nextInt();
+  System.out.print("Enter a positive integer: ");
+  num = input.nextInt();
 
-    while(num > 1){
-        for(int i = 10000; i <= 0; i--){
-            num -= num%i;
+  String num_str = "" + num;
+  num_length = num_str.length();
 
-        }
-    }
+  for(int a = 0; a < num_length; a++){
+
+    i = num_str.substring(a, a + 1);
+    num_final += Integer.valueOf(i);
+
+
+  }
+
+  System.out.println("The sum of the digits is: " + num_final);
+
+
 
     
-
-    
-    input.close();
+  input.close();
   }
 }
